5f7fcabc8ab1e973dad765b2505a447597af91ca
Polstuds27 <paul.mojal@gmail.com>
2026-05-01 23:09:54 +0800

    fixed folder structure & polished login ui

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -13,13 +13,13 @@ export default function LoginPage(){
   return (
     <>
       <div className="w-full h-full flex flex-col items-center">
-        <Link href="/" className="">Cafe Collective</Link>
+        <Link href="/" className="text-2xl mb-4 text-[#c08b4b] font-bold">Cafe Collective</Link>
 
         <div className="flex flex-col w-[350px] h-[500px] bg-[#442721d5] border-1 border-[#c08b4b94] rounded-xl pr-10 pl-10 pt-5 pb-5 items-center">
-            <h1>Welcome Back</h1>
-            <h2>Sign In with your google account.</h2>
+            <h1 className="text-xl font-semibold text-white mb-[3px]">Welcome Back</h1>
+            <h2 className="text-sm mb-4">Sign In with your google account.</h2>
 
-            <button className="flex w-full justify-center gap-2 items-center cursor-pointer bg-[#c08b4b] text-black p-2 rounded-2xl">
+            <button className="flex w-full justify-center gap-2 items-center cursor-pointer bg-[#c08b4b] text-black p-2 rounded-2xl mb-3">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -29,7 +29,7 @@ export default function LoginPage(){
                   Google
             </button>
 
-            <div className="flex items-center gap-3 w-full">
+            <div className="flex items-center gap-3 w-full mb-3">
               <div className="flex-1 h-px bg-[#c08b4b94]" />
               <span className="text-sm  font-light">Or continue with</span>
               <div className="flex-1 h-px bg-[#c08b4b94]" />
@@ -65,15 +65,28 @@ export default function LoginPage(){
                     {showPassword ? <EyeClosed className="w-4 h-4"/> : <Eye className="w-4 h-4"/>}
                   </button>
                 </div>
-                  <h2 className="text-xs text-end mt-1 hover:underline cursor-pointer">Forgot Password?</h2>
+                  <h2 className="text-xs text-end mt-1 hover:underline hover:text-[#c08b4b] cursor-pointer">Forgot Password?</h2>
               </span>
 
-              <button className="flex w-full justify-center gap-2 items-center cursor-pointer bg-[#c08b4b] text-black p-2 rounded-2xl">
+              <button className="flex w-full justify-center gap-2 items-center cursor-pointer bg-[#c08b4b] text-black p-2 rounded-2xl mb-2">
                 Sign In
-              </button>       
+              </button>
+
+              <p className="text-wrap text-center w-full text-xs">
+                Don't have an account yet? <Link href="/sign-up" className="underline text-[#c08b4b]">Register here.</Link>
+                </p>       
             </form>
 
         </div>
+
+        <span className="text-xs flex flex-wrap m-5">
+          By joining, you agree to our{" "}
+          <Link href="/terms" className="underline hover:text-[#c08b4b] mx-1">Terms of Service</Link>
+          and{" "}
+          <Link href="/privacy" className="underline hover:text-[#c08b4b] mx-1">Privacy Policy.</Link>
+          {" "}Let's keep the community respectful and helpful.
+</span>
+
       </div>
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins,  Inter, Lato, Cormorant_Garamond, Playfair_Display, DM_Serif_Display, Libre_Baskerville} from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -71,13 +68,9 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} h-full antialiased`}
     >
-      
         <body className="min-h-full flex flex-col bg-[#3E2723]">
-          <Navbar/>
           {children}
-          <Footer/>
         </body>
-
     </html>
   );
 }
